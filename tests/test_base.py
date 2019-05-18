d6c108f51b0a2a4d5374a1ea88a624cbcec5b2e4
Bhautik Joshi <bjoshi+github@gmail.com>
2019-05-17 23:40:31 -0700

    updated tests w/nose, temp files

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,19 +1,30 @@
 from unittest import TestCase
 import vrProjector
+import os
+import tempfile
 
 class TestBase(TestCase):
-  def test_root(self):
+  def test_test(self):
     self.assertTrue(True == True)
-    
-    
-# import vrProjector
-#
-# eq = vrProjector.EquirectangularProjection()
-# eq.loadImage("images/equirectangular.png")
-# # eq.set_use_bilinear(True)
-# cb = vrProjector.CubemapProjection()
-# cb.initImages(256,256)
-# cb.reprojectToThis(eq)
-# cb.saveImages("front.png", "right.png", "back.png", "left.png", "top.png", "bottom.png")
-#
-#
+
+  def test_equi_to_cube(self):    
+    with tempfile.TemporaryDirectory() as tmpdir:
+      eq = vrProjector.EquirectangularProjection()
+      eq.loadImage("images/equirectangular.png")
+      # eq.set_use_bilinear(True)
+      cb = vrProjector.CubemapProjection()
+      cb.initImages(256,256)
+      cb.reprojectToThis(eq)
+      cb.saveImages(os.path.join(tmpdir,"front.png"), 
+                    os.path.join(tmpdir,"right.png"), 
+                    os.path.join(tmpdir,"back.png"), 
+                    os.path.join(tmpdir,"left.png"), 
+                    os.path.join(tmpdir,"top.png"), 
+                    os.path.join(tmpdir,"bottom.png"))
+      self.assertTrue(os.path.exists(os.path.join(tmpdir,"front.png")))
+      self.assertTrue(os.path.exists(os.path.join(tmpdir,"right.png")))
+      self.assertTrue(os.path.exists(os.path.join(tmpdir,"back.png")))
+      self.assertTrue(os.path.exists(os.path.join(tmpdir,"left.png")))
+      self.assertTrue(os.path.exists(os.path.join(tmpdir,"top.png")))
+      self.assertTrue(os.path.exists(os.path.join(tmpdir,"bottom.png")))
+      
